02eb4eecc847d198f3e83fa339e7cd8d65c94f9a
vsonka <sonkarvikas0786@gmail.com>
2018-06-11 22:32:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-var time = require('time');
+'use strict';
+
 exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
-    callback(null, {
-        statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+    var webdriver = require('selenium-webdriver');
+    var chrome = require('selenium-webdriver/chrome');
+    var builder = new webdriver.Builder().forBrowser('chrome');
+    var chromeOptions = new chrome.Options();
+    const defaultChromeFlags = [
+        '--headless',
+        '--disable-gpu',
+        '--window-size=1280x1696', // Letter size
+        '--no-sandbox',
+        '--user-data-dir=/tmp/user-data',
+        '--hide-scrollbars',
+        '--enable-logging',
+        '--log-level=0',
+        '--v=99',
+        '--single-process',
+        '--data-path=/tmp/data-path',
+        '--ignore-certificate-errors',
+        '--homedir=/tmp',
+        '--disk-cache-dir=/tmp/cache-dir'
+    ];
+
+    chromeOptions.setChromeBinaryPath("/var/task/lib/chrome");
+    chromeOptions.addArguments(defaultChromeFlags);
+    builder.setChromeOptions(chromeOptions);
+
+    var driver = builder.build();
+    driver.get(event.url);
+    driver.getTitle().then(function(title) {
+
+        console.log("Page title for " + event.url + " is " + title)
+        callback(null, 'Page title for ' + event.url + ' is ' + title);
     });
+
+    driver.quit();
 };
+
+
